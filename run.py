16f6fe2693e25d2dbeb4c7b89f7543b9411fdf5d
AnnieeeeeF <c7fan@ucsd.edu>
2022-12-03 08:41:01 -0800

    added run.py

--- run.py
+++ run.py
@@ -2,15 +2,40 @@ import os
 import json
 import sys
 import pandas as pd
-from model import build_svm
+from src.relevance_model import task1_models
+from src.sentiment_model import task2_models
+from src.data import load_csv
+
+def main(targets):
+    fp = os.path.join(targets[0], 'data.csv')
+    df_raw = pd.read_csv(fp)
+    try:
+        df = load_csv(df_raw)
+    except Exception as e:
+        print('Failed to clean dataset')
+        print(e)
+
+    if 'doc2vec' in targets or 'Doc2Vec' in targets:
+        feature == 'Doc2Vec'
+    else:
+        feature == 'TF-IDF'
+
+    with open('config/task1-params.json') as fh:
+        task1_params = json.load(fh)
+    with open('config/task2-params.json') as fh:
+        task2_params = json.load(fh)
+
+    try:
+        out1 = task1_models(df, feature, task1_params['svm'], task1_params['dt'])
+        out2 = task2_models(df, feature, task1_params['ridge'], task2_params['dt'])
+    except Exception as e:
+        print('Encountered error when running models')
+        print(e)
+
+    return [out1, out2]
+
 
 if __name__ == '__main__':
 
-    target = sys.argv[1]
-    fp = os.path.join(target, 'data.csv')
-    df = pd.read_csv(fp)
-
-    with open('config.json') as fh:
-            params = json.load(fh)
-
-    build_svm(df, **params)
+    targets = sys.argv[1:]
+    main(targets)
